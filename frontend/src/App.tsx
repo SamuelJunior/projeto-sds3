e5351fb4a4633a9ebd3225ce0ad07723db0b0877
SamuelJunior <smljr7@gmail.com>
2021-09-14 23:04:38 -0300

    Componentes estaticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import Footer from "assets/css/components/Footer";
+import NavBar from "assets/css/components/NavBar";
+import DataTable from "assets/css/components/DataTable";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
